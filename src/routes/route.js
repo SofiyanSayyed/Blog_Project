52ab1bf262f3716746ed34c050539a830f44aa97
Sufiyan Sayyed <sayyedsufiyan1301@gmail.com>
2023-05-31 21:40:35 +0530

    almost completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,63 @@
 const express = require('express');
-const route = express.Router();
+const router = express.Router();
+
+const authorController = require("../controllers/authorController");
+const blogController = require("../controllers/blogController");
+
+const verifyMiddleware = require("../middlewares/verifyMiddleware");
+const jwtMiddleware = require("../middlewares/jwtMiddleware");
+
+router.post("/authors", authorController.createAuthor);
+
+router.post(
+    "/login",
+    jwtMiddleware.verifyEmailPass,
+    authorController.authorLogin
+);
+
+router.post(
+    "/blogs",
+    verifyMiddleware.authorChecker,
+    jwtMiddleware.verifytoken,
+    blogController.createBlog
+);
+
+router.get(
+    "/blogs",
+    verifyMiddleware.verifyId,
+    jwtMiddleware.verifytoken,
+    blogController.getBlogs
+);
+
+router.put(
+    "/blogs/:blogId",
+    verifyMiddleware.verifyId,
+    jwtMiddleware.verifytoken,
+    jwtMiddleware.authorizedAuthor,
+    blogController.updateBlogs
+);
+
+router.delete(
+    "/blogs/:blogId",
+    verifyMiddleware.verifyId,
+    jwtMiddleware.verifytoken,
+    jwtMiddleware.authorizedAuthor,
+    blogController.deleteBlog
+);
+
+router.delete(
+    "/blogs",
+    verifyMiddleware.verifyId,
+    jwtMiddleware.verifytoken,
+    jwtMiddleware.authorizedAuthor,
+    blogController.deleteBlogsByQuery
+);
 
 
 
 
-
-
-route.get('*', async function(req, res) {
+router.get('*', async function(req, res) {
     return res.send("Welcome")
 });
 
-module.exports = route;
+module.exports = router;
